65a9a0e5b056aa9d1689fcacca1e18aea6047610
Misaka <chuxubank@qq.com>
2024-11-05 08:47:49 +0800

    Scripted auto-commit on change (2024-11-05 08:47:49) by gitwatch.sh

--- test.el
+++ test.el
@@ -1,11 +1,22 @@
-(defun my-org-horizontal-block (backend)
-  (let ((attrs '((:html . "<div style=\"display: flex; justify-content: space-around;\">")
-                 (:latex . "\\begin{minipage}{0.25\\textwidth}"))))
-    (dolist (block (org-element-map (org-element-parse-buffer) 'special-block
-                     (lambda (b) (when (string= (org-element-property :type b) "abcd") b))))
-      (let ((contents (org-element-interpret-data (org-element-contents block))))
-        (pcase backend
-          (`html (format "%s\n%s\n</div>" (alist-get :html attrs) contents))
-          (`latex (format "%s\n%s\n\\end{minipage}" (alist-get :latex attrs) contents)))))))
+(defun my-org-option-block-transform (backend)
+  "Transform #+begin_option blocks to horizontal display for HTML and LaTeX export."
+  (when (memq backend '(html latex))
+    (let ((case-fold-search t))
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward "^#\\+begin_option\\s-*\n\\(\\(?:- .*\\(?:\n\\|\\'\\)\\)+\\)#\\+end_option" nil t)
+          (let* ((content (match-string 1))
+                 (options (split-string content "\n" t))
+                 (labels (mapcar (lambda (n) (char-to-string (+ n 65))) (number-sequence 0 (1- (length options)))))
+                 (formatted-options (mapconcat (lambda (opt)
+                                                 (concat (pop labels) ". " (string-trim opt)))
+                                               options " ")))
+            (replace-match
+             (if (eq backend 'html)
+                 ;; HTML 输出格式，包含内联 CSS 样式
+                 (format "<div style=\"display: flex; flex-wrap: wrap; gap: 1em;\">%s</div>" formatted-options)
+               ;; LaTeX 输出格式，使用 \optionblock 宏
+               (format "\\optionblock{%s}" formatted-options))
+             t t)))))))
 
-(add-hook 'org-export-before-parsing-hook 'my-org-horizontal-block)
+(add-hook 'org-export-before-parsing-hook #'my-org-option-block-transform)
