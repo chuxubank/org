3ce9356c006dd024b8b4ac2248dcb4b64215f090
Misaka <chuxubank@qq.com>
2024-11-05 08:35:59 +0800

    Scripted auto-commit on change (2024-11-05 08:35:58) by gitwatch.sh

---
+++ test.el
@@ -0,0 +1,11 @@
+(defun my-org-horizontal-block (backend)
+  (let ((attrs '((:html . "<div style=\"display: flex; justify-content: space-around;\">")
+                 (:latex . "\\begin{minipage}{0.25\\textwidth}"))))
+    (dolist (block (org-element-map (org-element-parse-buffer) 'special-block
+                     (lambda (b) (when (string= (org-element-property :type b) "horizontal") b))))
+      (let ((contents (org-element-interpret-data (org-element-contents block))))
+        (pcase backend
+          (`html (format "%s\n%s\n</div>" (alist-get :html attrs) contents))
+          (`latex (format "%s\n%s\n\\end{minipage}" (alist-get :latex attrs) contents)))))))
+
+(add-hook 'org-export-before-parsing-hook 'my-org-horizontal-block)
