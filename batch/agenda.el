a62558e05db6f2da6510f7a903598dc0f119aca1
Misaka <chuxubank@qq.com>
2025-04-30 16:54:51 +0800

    Scripted auto-commit on change (2025-04-30 16:54:51) by gitwatch.sh

--- batch/agenda.el
+++ batch/agenda.el
@@ -2,6 +2,8 @@
 (require 'org-agenda)
 (require 'htmlize)
 (setq
+ org-directory "/root/org"
  org-agenda-files (list "/root/org/")
+ org-agenda-include-diary nil
  org-agenda-custom-commands
  '(("X" agenda "" nil ("agenda.html"))))
