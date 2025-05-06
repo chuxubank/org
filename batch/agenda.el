2f43f796b264f51cd2e0baa8a64d87098c8641b0
Misaka <chuxubank@qq.com>
2025-05-06 14:19:55 +0800

    Scripted auto-commit on change (2025-05-06 14:19:55) by gitwatch.sh

--- batch/agenda.el
+++ batch/agenda.el
@@ -10,10 +10,22 @@
  org-agenda-skip-scheduled-if-done t
  org-agenda-skip-deadline-if-done t
  org-agenda-skip-deadline-prewarning-if-scheduled t
- org-agenda-prefix-format '((agenda . " %i %-20:c%?-12t% s")
+ org-agenda-prefix-format '((agenda . " %i %-20:c%?-20t% s")
                             (todo   . " %i %-20:c")
                             (tags   . " %i %-20:c")
                             (search . " %i %-20:c"))
+ org-todo-keywords '((sequence
+                      "TODO(t)"  ; A task that needs doing & is ready to do
+                      "PROJ(p)"  ; A project, which usually contains other tasks
+                      "LOOP(r)"  ; A recurring task
+                      "STRT(s)"  ; A task that is in progress
+                      "WAIT(w)"  ; Something external is holding up this task
+                      "HOLD(h)"  ; This task is paused/on hold because of me
+                      "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+                      "|"
+                      "DONE(d)"  ; Task successfully completed
+                      "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+                     )
  org-agenda-custom-commands
  '(("X" agenda "" nil ("publish/agenda.html"))
    ("Y" alltodo "" nil ("publish/todo.html"))))
