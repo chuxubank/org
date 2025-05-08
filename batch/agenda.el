29d82b529cf512f9cbf1c343e8914ad3ef207726
Misaka <chuxubank@qq.com>
2025-05-08 13:51:34 +0800

    Scripted auto-commit on change (2025-05-08 13:51:34) by gitwatch.sh

--- batch/agenda.el
+++ batch/agenda.el
@@ -1,7 +1,6 @@
 (package-initialize)
 
 (use-package org
-  :load-path "~/.emacs.d/elpa/org-mode/lisp"
   :custom
   (org-directory (getenv "ORG_DIR"))
   (org-agenda-files (list (getenv "ORG_DIR")))
@@ -37,12 +36,10 @@
   :ensure t)
 
 (use-package org-habit
-  :load-path "~/.emacs.d/elpa/org-mode/lisp"
   :custom
   (org-habit-graph-column 60))
 
 (use-package org-agenda
-  :load-path "~/.emacs.d/elpa/org-mode/lisp"
   :custom
   (org-agenda-include-diary t)
   (org-agenda-skip-scheduled-if-done t)
@@ -56,6 +53,4 @@
   (org-agenda-custom-commands
    '(("X" agenda "" nil ("publish/agenda.html"))
      ("Y" alltodo "" nil ("publish/todo.html"))))
-  (org-agenda-export-html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"assets/style.css\">")
-  :config
-  (org-batch-store-agenda-views))
+  (org-agenda-export-html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"assets/style.css\">"))
