488963da9ee908231b6b1fd141f736bf156ee99b
Misaka <chuxubank@qq.com>
2025-05-23 11:51:27 +0800

    Scripted auto-commit on change (2025-05-23 11:51:27) by gitwatch.sh

--- batch/agenda.el
+++ batch/agenda.el
@@ -59,7 +59,7 @@
    '(("X" agenda "" ((org-agenda-span 14)) ("publish/agenda.html"))
      ("Y" alltodo "" nil ("publish/todo.html"))))
   (org-agenda-current-time-string
-   "← now ───────────────────────────────────────────────")
+   "← now ───────────────────────────────")
   (org-agenda-export-html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"assets/style.css\">"))
 
 (use-package bbdb
